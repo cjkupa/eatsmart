472656eba5373387681a9b11bddfc263da278d6a
Koops <koops@Koopss-MacBook-Pro.local>
2026-06-03 06:56:47 +1200

    Force scrollbar to prevent resize

--- src/EatSmart.jsx
+++ src/EatSmart.jsx
@@ -192,7 +192,7 @@ export default function EatSmart() {
     const link = document.createElement('link');
     link.href = 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap';
     link.rel = 'stylesheet';
-    document.head.appendChild(link); document.documentElement.style.overflowY = "scroll";
+    document.head.appendChild(link); document.documentElement.style.overflowY = "scroll"; document.documentElement.style.overflowX = "hidden"; document.documentElement.style.overflowY = "scroll";
     document.body.style.overflowX = "hidden";
     document.documentElement.style.overflowX = "hidden";
     document.body.style.overflowX = "hidden";
